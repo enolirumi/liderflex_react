eb547b28b25f99d195cc0c253518975057173737
Enzo Russo <enzorussomiranda@gmail.com>
2025-08-05 07:33:34 -0300

    a

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styles from './style.module.scss'; // se estiver usando CSS Modules
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
+import WhatsAppButton from '../../Components/WhatsAppButton/WhatsAppButton';
 
 export default function About() {
     return (
         <>
             <Header />
+            <WhatsAppButton />
             <div className={styles.backgroundLightsContainer}>
                 <div className={styles.first}>
 
@@ -25,22 +27,27 @@ export default function About() {
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis odio maiores, minima architecto aspernatur nostrum facilis soluta fugiat accusantium distinctio nesciunt commodi tenetur dolor accusamus dolorem rerum quas, voluptatum excepturi.
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit libero laboriosam delectus labore, magnam distinctio, ipsa in reprehenderit minus tempora neque exercitationem cum eaque enim, iste corrupti? Aliquam, omnis earum!
-                            
+
                         </p>
                     </div>
                     <div className={`${styles.cardServices} ${styles.infoCard} ${styles.card}`}>
-                        <div>
+                        <div className={styles.data}>
                             1.000+
                         </div>
-                        <div>
-                            clientes satisfeitos
+                        <div className={styles.cardDescription}>
+                            Clientes satisfeitos
                         </div>
                     </div>
                     <div className={`${styles.cardImage} ${styles.card}`}>
 
                     </div>
                     <div className={`${styles.cardA} ${styles.infoCard} ${styles.card}`}>
-
+                        <div className={styles.data}>
+                            20+
+                        </div>
+                        <div className={styles.cardDescription}>
+                            Anos de mercado
+                        </div>
                     </div>
                     <div className={`${styles.cardB} ${styles.infoCard} ${styles.card}`}>
 
--- src/Pages/Doubts/Doubts.jsx
+++ src/Pages/Doubts/Doubts.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import styles from './style.module.scss'; // se estiver usando CSS Modules
 
 import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
 
 export default function Doubts() {
     return (
         <>
             <Header />
-            <div className={styles.container}>
-                <h1>Bem-vindo à Dúvidas</h1>
-                <p>Esta é a página inicial do seu site.</p>
-            </div>
+            
+            <main>
+                
+            </main>
+
+            <Footer />
         </>
     );
 }
