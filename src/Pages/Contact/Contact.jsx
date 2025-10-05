1af14c080b42a5991f8bce7a6d08a98caded10bf
Enzo Russo <82696843+enolirumi@users.noreply.github.com>
2025-10-05 10:00:49 -0300

    disparo formulario

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { ToastContainer, toast, Bounce } from 'react-toastify';
+import axios from 'axios';
 import styles from './style.module.scss'; // se estiver usando CSS Modules
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
@@ -10,6 +12,48 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function Contact() {
     const formRef = useRef(null)
     const cardsRef = useRef(null)
+    const [form, setForm] = useState({
+        nome: "",
+        sobrenome: "",
+        email: "",
+        telefone: "",
+        tipoContato: "",
+        mensagem: "",
+    })
+
+    const [status, setStatus] = useState("Enviar")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        
+        setStatus("Enviando...")
+
+        console.log(form);
+        
+
+        try {
+            const response = await axios.post("https://liderflexvidros.com.br/api/sendMail.php", form, {
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            });
+
+            if(response.data.status == "success") {
+
+            } else {
+
+            }
+            
+        } catch (error) {
+            
+        }
+
+        setStatus("Enviar");
+    }
+
+    const handleChangeInput = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
 
     const ableInput = (ev) => {
         ev.target.parentNode.classList.add(styles.able)
@@ -18,6 +62,16 @@ export default function Contact() {
     const disableInput = (ev) => {
         if (ev.target.value == "") {
             ev.target.parentNode.classList.remove(styles.able)
+            let required
+            if(required = ev.target.parentNode.querySelector(".requiredAsterisco")) {
+                if(required.classList.contains("visibleMessage")) {
+                    required.classList.remove("visibleMessage")
+                } else {
+                    required.classList.add("visibleMessage")
+                }
+            }
+
+            
         }
     }
 
@@ -37,45 +91,58 @@ export default function Contact() {
         <>
             <Header />
             <main className={styles.main}>
+                <ToastContainer
+                    position="top-right"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={true}
+                    closeOnClick={false}
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover={true}
+                    theme="dark"
+                    transition={Bounce}
+                />
                 <LightsBg></LightsBg>
                 <div className={styles.formContainer} ref={formRef}>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <h3>Como podemos ajudar?</h3>
                         <div className={styles.singleInputContainer}>
                             <label htmlFor="">Tipo de contato</label>
-                            <select name="" id="" onFocus={(ev) => { ableInput(ev) }} onBlur={(ev) => { disableInput(ev) }}>
+                            <select name="tipoContato" id="" onChange={handleChangeInput} onFocus={(ev) => { ableInput(ev) }} onBlur={(ev) => { disableInput(ev) }}>
                                 <option value=""></option>
                                 <option value="1">Realizar orçamento</option>
-                                <option value="3">Dúvida</option>
+                                <option value="2">Dúvida</option>
                                 <option value="3">Problemas pós instalação</option>
-                                <option value="2">Reclamação</option>
-                                <option value="3">Elogio</option>
+                                <option value="4">Reclamação</option>
+                                <option value="5">Elogio</option>
                             </select>
                         </div>
                         <div className={styles.doubleInputContainer}>
                             <div>
                                 <label htmlFor="name_form">Nome <span className={styles.requiredAsterisco}>*</span></label>
-                                <input type="text" id="name_form" name='name_form' onFocus={(ev) => { ableInput(ev) }} onBlur={(ev) => { disableInput(ev) }} />
+                                <input type="text" name="nome" onChange={handleChangeInput} id="name" onFocus={(ev) => { ableInput(ev) }} onBlur={(ev) => { disableInput(ev) }} />
                             </div>
                             <div>
                                 <label htmlFor="">Sobrenome <span className={styles.requiredAsterisco}>*</span></label>
-                                <input type="text" onFocus={(ev) => { ableInput(ev) }} onBlur={(ev) => { disableInput(ev) }} />
+                                <input type="text" name="sobrenome" onChange={handleChangeInput} onFocus={(ev) => { ableInput(ev) }} onBlur={(ev) => { disableInput(ev) }} />
                             </div>
                         </div>
                         <div className={styles.singleInputContainer}>
                             <label htmlFor="">E-mail <span className={styles.requiredAsterisco}>*</span></label>
-                            <input type="email" onFocus={(ev) => { ableInput(ev) }} onBlur={(ev) => { disableInput(ev) }} />
+                            <input type="email" name="email" onChange={handleChangeInput} onFocus={(ev) => { ableInput(ev) }} onBlur={(ev) => { disableInput(ev) }} />
                         </div>
                         <div className={styles.singleInputContainer}>
                             <label htmlFor="">Telefone <span className={styles.requiredAsterisco}>*</span></label>
-                            <input type="email" onFocus={(ev) => { ableInput(ev) }} onBlur={(ev) => { disableInput(ev) }} />
+                            <input type="tel" name="telefone" onChange={handleChangeInput} onFocus={(ev) => { ableInput(ev) }} onBlur={(ev) => { disableInput(ev) }} />
                         </div>
                         <div className={styles.singleInputContainer}>
                             <label htmlFor="">Mensagem <span className={styles.requiredAsterisco}>*</span></label>
-                            <textarea name="" id="" onFocus={(ev) => { ableInput(ev) }} onBlur={(ev) => { disableInput(ev) }}></textarea>
+                            <textarea name="mensagem" id="" onChange={handleChangeInput} onFocus={(ev) => { ableInput(ev) }} onBlur={(ev) => { disableInput(ev) }}></textarea>
                         </div>
                         <div className={styles.singleInputContainer}>
-                            <button type='submit'>Enviar</button>
+                            <button type='submit'>{status}</button>
                         </div>
                         <div className={styles.singleInputContainer}>
                             <a href="https://wa.me/message/LWCQ4Q6S4IYAG1" target="_blank" className={styles.wppFormBtn}>Ou nos chame no <FaWhatsapp />WhatsApp</a>
